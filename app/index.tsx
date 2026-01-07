225e0e0258c07d1d6ef6c51fa2e16717668f7347
sarochachiamcharoen-gif <sarocha.chiamcharoen@e-tech.ac.th>
2026-01-07 11:50:43 +0700

    เเก้ไข

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import { useRouter } from 'expo-router'
 import React from 'react'
 import { Button, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
     const router= useRouter()
   return (
-    <View>
+    <SafeAreaView style={{gap:5,marginTop:20,alignItems:'center'}}>
       <Text>index</Text>
       <Button title='หน้าคำนวณพื้นที่สี่เหลี่ยมจัตุรัส' onPress={() => router.navigate("/Cal")}/>
       <Button title='หน้าคำนวณกิโลเมตรเป็นไมล์' onPress={() => router.navigate("/Link")}/>
-    </View>
+    </SafeAreaView>
   )
 }
